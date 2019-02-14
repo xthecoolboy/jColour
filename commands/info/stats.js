18dff454b25cf629c3859b68a75a9955d344e698
jaqreven <kopsukkayt@gmail.com>
2019-02-14 20:12:15 +0200

    (bad) sharding support for stats command

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -1,8 +1,17 @@
-const {Command} = require('discord.js-commando');
-const {stripIndents} = require('common-tags');
+const {
+  Command
+} = require('discord.js-commando');
+const {
+  stripIndents
+} = require('common-tags');
 
-const {version, description} = require('./../../package.json');
-const {formatMs} = require("./../../tools/formatms.js");
+const {
+  version,
+  description
+} = require('./../../package.json');
+const {
+  formatMs
+} = require("./../../tools/formatms.js");
 
 const os = require('os');
 const Discord = require('discord.js');
@@ -17,8 +26,8 @@ module.exports = class ChannelCommand extends Command {
       examples: ["stats", "stats adding arguments is useless"],
       guildOnly: false,
       throttling: {
-          usages: 2,
-          duration: 10
+        usages: 2,
+        duration: 10
       },
       aliases: [
         "info"
@@ -48,7 +57,7 @@ module.exports = class ChannelCommand extends Command {
 
       .addField(
         "Specs",
-        stripIndents`
+        stripIndents `
         **CPU**: ${os.cpus().length}x ${(os.cpus()[0]["speed"] / 1000).toFixed(2)}GHz
         **RAM**: ${(os.totalmem() / (1024 ** 3)).toFixed(1)}gb`,
         true
@@ -56,27 +65,70 @@ module.exports = class ChannelCommand extends Command {
 
       .addField(
         "Versions",
-        stripIndents`
+        stripIndents `
         **Bot**: v${version} (${description})
         **NodeJS**: ${process.version}`,
         true
       )
 
-      .addField(
+    embed.setFooter("Owner: " + client.owners[0].tag)
+
+    if (client.shard.count === 1) { // if theres only one shard it doesnt spam useless info
+      embed.addField(
         "Size",
-        stripIndents`
-        **Guilds**: ${client.guilds.size * client.shard.count}
-        **Channels**: ${client.channels.size * client.shard.count}
-        **Shards**: ${client.shard.count}`,
+        stripIndents `
+          **Shards**: ${client.shard.count}
+          **Guilds**: ${client.guilds.size}
+          **Channels**: ${client.channels.size}
+          **Users**: ${client.users.size}`,
         true
       )
-
-      .addField(
-        "Owner",
-        this.client.owners[0].tag,
+      msg.embed(embed)
+    } else { // if there are multiple shards, you get info for both the current shard and all shards together
+      embed.addField(
+        "This Shard",
+        stripIndents `
+          **Shard ID**: ${client.shard.id}
+          **Guilds**: ${client.guilds.size}
+          **Channels**: ${client.channels.size}
+          **Users**: ${client.users.size}`,
         true
-      )
+      );
 
-    msg.embed(embed)
+      getShardValues()
+        .then(results => {
+          embed.addField(
+            "All Shards",
+            stripIndents `
+              **Shards**: ${client.shard.count}
+              **Guilds**: ${results[0]}
+              **Channels**: ${results[1]}
+              **Users**: ${results[2]}`,
+            true
+          )
+          msg.embed(embed)
+        })
+
+
+      function getShardValues(callback) { // this part sucks and i dont understand it
+        return new Promise(function(fulfill, reject) {
+          client.shard.fetchClientValues('guilds.size')
+            .then(count => {
+              let totalGuilds = count.reduce((prev, val) => prev + val, 0)
+              client.shard.fetchClientValues('channels.size')
+                .then(count => {
+                  let totalChannels = count.reduce((prev, val) => prev + val, 0)
+                  client.shard.fetchClientValues('users.size')
+                    .then(count => {
+                      let totalUsers = count.reduce((prev, val) => prev + val, 0)
+                      fulfill([totalGuilds, totalChannels, totalUsers]);
+                    })
+                })
+            })
+        })
+      }
+
+
+    }
   }
 };
